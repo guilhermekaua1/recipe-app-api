bc4485b76e8a5783f6d467c9dacd66b38e075af1
Guilherme <guilhermekaua10@gmail.com>
2023-08-31 16:37:28 -0300

    Adding user model tests and normalizing email addresses

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "core.User"
